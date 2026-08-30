e6085437f1c88ae7e3116558c2a61f62445ca722
shakilaaulia <110599468+shakilaaulia@users.noreply.github.com>
2026-08-30 15:48:56 +0700

    hapus

--- app/page.tsx
+++ app/page.tsx
@@ -8,24 +8,21 @@ import AwardsSection from "@/components/sections/AwardsSection";
 import ProjectsSection from "@/components/sections/ProjectsSection";
 import ContactSection from "@/components/sections/ContactSection";
 import Footer from "@/components/sections/Footer";
-import { Pointer } from "@/components/ui/custom-cursor";
 
 export default function Home() {
   return (
     <SmoothScroll>
       <Preloader />
       <Navbar />
-      <Pointer name="Shakila" className="w-full min-h-screen">
-        <main className="flex flex-col w-full min-h-screen">
-          <HeroSection />
-          <AboutSection />
-          <ExperienceSection />
-          <AwardsSection />
-          <ProjectsSection />
-          <ContactSection />
-        </main>
-        <Footer />
-      </Pointer>
+      <main className="flex flex-col w-full min-h-screen">
+        <HeroSection />
+        <AboutSection />
+        <ExperienceSection />
+        <AwardsSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
+      <Footer />
     </SmoothScroll>
   );
 }
